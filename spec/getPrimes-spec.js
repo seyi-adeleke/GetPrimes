5e022b68d4a67f1386ad11e664f6eda1d41ddd9f
Seyi Adeleke <seyiadeleke42@yahoo.com>
2017-05-08 21:08:08 +0100

    added more tests

--- spec/getPrimes-spec.js
+++ spec/getPrimes-spec.js
@@ -5,5 +5,59 @@ describe("getPrimes", function () {
         let primes = myPrimes.getPrimes(5);
         expect(primes).toBe([2,3,5]);
     });
+    it("should return [ 2, 3, 5,7 ]", function () {
+        let primes = myPrimes.getPrimes(9);
+        expect(primes).toBe([2,3,5,7]);
+    });
+    it("should return [ 2, 3, 5,7 ]", function () {
+        let primes = myPrimes.getPrimes(9);
+        expect(primes).toBe([2,3,5,7]);
+    });
+    it("should return [ 2, 3, 5, 7, 11, 13 ] ", function () {
+        let primes = myPrimes.getPrimes(16);
+        expect(primes).toBe([ 2, 3, 5, 7, 11, 13 ] );
+    });
+
+
+});
+describe("getPrimes", function () {
+    it("should return [] if number is 0", function () {
+        let primes = myPrimes.getPrimes("string");
+        expect(primes).toBe([]);
+    });
+
+
+
+});
+describe("getPrimes", function () {
+
+    it("should return [] if no prime number is attainable", function () {
+        let primes = myPrimes.getPrimes();
+        expect(primes).toBe([]);
+    });
+
+});
+describe("getPrimes", function () {
+    it("should return [] if number is less than 0", function () {
+        let primes = myPrimes.getPrimes(-2);
+        expect(primes).toBe([]);
+    });
+
+    it("should return [] if number is less than 0", function () {
+        let primes = myPrimes.getPrimes(-100);
+        expect(primes).toBe([]);
+    });
+});
+
+describe("getPrimes", function () {
+    it("should return [] if number not an integer", function () {
+        let primes = myPrimes.getPrimes("string");
+        expect(primes).toBe([]);
+    });
+    it("should return [] if number not an integer", function () {
+        let primes = myPrimes.getPrimes([]);
+        expect(primes).toBe([]);
+    });
+
 
 });
