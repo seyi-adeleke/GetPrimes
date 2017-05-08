faf33d2d9e086b2461491fb45472ed16ed0b8a70
Seyi Adeleke <seyiadeleke42@yahoo.com>
2017-05-08 23:11:16 +0100

    changed toBe method to toEqual(bug fix)

--- spec/getPrimes-spec.js
+++ spec/getPrimes-spec.js
@@ -3,27 +3,27 @@ const myPrimes = require("../app/getPrimes");
 describe("getPrimes", function () {
     it("should return [ 2, 3, 5 ]", function () {
         let primes = myPrimes.getPrimes(5);
-        expect(primes).toBe([2,3,5]);
+        expect(primes).toEqual([2,3,5]);
+
     });
     it("should return [ 2, 3, 5,7 ]", function () {
         let primes = myPrimes.getPrimes(9);
-        expect(primes).toBe([2,3,5,7]);
+        expect(primes).toEqual([2,3,5,7]);
     });
     it("should return [ 2, 3, 5,7 ]", function () {
         let primes = myPrimes.getPrimes(9);
-        expect(primes).toBe([2,3,5,7]);
+        expect(primes).toEqual([2,3,5,7]);
     });
     it("should return [ 2, 3, 5, 7, 11, 13 ] ", function () {
         let primes = myPrimes.getPrimes(16);
-        expect(primes).toBe([ 2, 3, 5, 7, 11, 13 ] );
+        expect(primes).toEqual([ 2, 3, 5, 7, 11, 13 ] );
     });
-
-
 });
+
 describe("getPrimes", function () {
     it("should return [] if number is 0", function () {
         let primes = myPrimes.getPrimes("string");
-        expect(primes).toBe([]);
+        expect(primes).toEqual([]);
     });
 
 
@@ -33,31 +33,29 @@ describe("getPrimes", function () {
 
     it("should return [] if no prime number is attainable", function () {
         let primes = myPrimes.getPrimes();
-        expect(primes).toBe([]);
+        expect(primes).toEqual([]);
     });
 
 });
 describe("getPrimes", function () {
     it("should return [] if number is less than 0", function () {
         let primes = myPrimes.getPrimes(-2);
-        expect(primes).toBe([]);
+        expect(primes).toEqual([]);
     });
 
     it("should return [] if number is less than 0", function () {
         let primes = myPrimes.getPrimes(-100);
-        expect(primes).toBe([]);
+        expect(primes).toEqual([]);
     });
 });
 
 describe("getPrimes", function () {
     it("should return [] if number not an integer", function () {
         let primes = myPrimes.getPrimes("string");
-        expect(primes).toBe([]);
+        expect(primes).toEqual([]);
     });
     it("should return [] if number not an integer", function () {
         let primes = myPrimes.getPrimes([]);
-        expect(primes).toBe([]);
+        expect(primes).toEqual([]);
     });
-
-
 });
