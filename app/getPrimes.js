ccdcf63284351c2b2249c21e7a30bae07aed3895
Seyi Adeleke <seyiadeleke42@yahoo.com>
2017-05-08 20:51:41 +0100

    created get primes function

---
+++ app/getPrimes.js
@@ -0,0 +1,33 @@
+'use strict';
+
+
+exports.getPrimes = function(n){
+    let primes=[];
+
+    for(let i=0;i<=n;i++){
+        if(isPrime(i)){
+            primes.push(i);
+        }
+    }
+    return primes;
+};
+
+
+
+const isPrime = (number) =>{
+    if (number == 2){
+        return true
+    }
+    if (number < 2 || number % 2 == 0){
+        return false
+    }
+
+    for (var i = 2; i <= Math.sqrt(number); i++){
+        if (number % i == 0) {
+            return false;
+        }
+    }
+
+    return true;
+};
+
